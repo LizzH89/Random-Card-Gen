bf9255c7f9f017d28a1649a17f0281c9077047c7
LizzH89 <lizziehidalgoleiva@gmail.com>
2021-02-18 03:42:27 +0000

    this is the most complex thing I have ever done

--- src/main.js
+++ src/main.js
@@ -2,9 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//*window.onload = function() {};
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const palos = ["\u2665", "\u2666", "\u2663", "\u2660"];
+//random del nuemro;
+const randomn = Math.floor(Math.random() * values.length);
+//random del palo;
+let randomp = Math.floor(Math.random() * palos.length);
+let pp = palos[randomp];
+console.log(pp);
+document.querySelector(".numero").innerHTML = values[randomn];
+document.querySelector(".palo-arriba").innerHTML = palos[randomp];
+document.querySelector(".palo-abajo").innerHTML = palos[randomp];
+document.querySelector(".palo-abajo").style.color = colores(pp);
+document.querySelector(".palo-arriba").style.color = colores(pp);
+document.querySelector(".numero").style.color = colores(pp);
+
+function colores(palo) {
+  if (pp == "\u2665" || pp == "\u2666") {
+    return "red";
+  } else {
+    return "black";
+  }
+}
